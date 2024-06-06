1d01052289bbdd4637b92ec0264a01250440d46e
Dev-Asys-Ferolino <fjay.ferolino@mlhuillier.com>
2024-06-06 11:04:21 +0800

    update app.module

--- week-2/todo-list/src/app.module.ts
+++ week-2/todo-list/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { TasksModule } from './tasks/tasks.module';
 import { TaskModule } from './task/task.module';
 import { TaskController } from './task/task.controller';
-// import { TaskModule } from './task/task.module';
 import { TaskService } from './task/task.service';
 import { PrismaService } from 'prisma/prisma/prisma.service';
 
