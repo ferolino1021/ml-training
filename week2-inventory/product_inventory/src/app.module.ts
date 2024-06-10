ebddf972f12b5c35adc574deae69a9ea6dbaaa16
Dev-Asys-Ferolino <fjay.ferolino@mlhuillier.com>
2024-06-10 08:51:29 +0800

    products

---
+++ week2-inventory/product_inventory/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+import { PrismaService } from 'prisma/prisma/prisma.service';
+
+@Module({
+  imports: [ProductModule],
+  controllers: [AppController, ProductController],
+  providers: [AppService, ProductService, PrismaService],
+})
+export class AppModule {}
